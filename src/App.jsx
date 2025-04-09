005164e6136695ed9c1c138633895d7b5cc8d479
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-04-09 18:00:01 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='h-50 text-4xl text-green-700 '>Hello</h1>
+      <h1 className='h-50 text-4xl text-green-700 '>Hello From Shriharsh</h1>
     </>
   )
 }
