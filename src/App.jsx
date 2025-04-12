6db12ba60820b70f2770e547e0ae526a9ef2fa6a
Pritam Mandal <pritampmandal@gmail.com>
2025-04-12 11:52:44 +0530

    working on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <>
-      <h1 className='h-50 text-4xl text-green-700 '>Hello</h1>
+     <Home/>
     </>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+     <div className='h-[50vh] w-ful mt-15'>
+
+     </div>
+     <div>
+
+     </div>
+    </>
+  );
+}
+
+export default Home;
